65f989b9f2ea5ed689ce3fc7c2fc193e5348a196
Sevval Erdal <sevvalerdal08@gmail.com>
2023-06-09 16:59:52 +0300

    Walking SFX script and effects added

--- Survive Titanic Mac/Assets/WalkingSFX.cs
+++ Survive Titanic Mac/Assets/WalkingSFX.cs
@@ -4,15 +4,62 @@ using UnityEngine;
 
 public class WalkingSFX : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public enum TerrainType
     {
-        
+        Grass,
+        Wood,
+        Sand,
+        Water,
+        // add more as needed
     }
 
-    // Update is called once per frame
-    void Update()
+    [SerializeField]
+    private AudioClip grassWalk;
+    [SerializeField]
+    private AudioClip woodWalk;
+    [SerializeField]
+    private AudioClip sandWalk;
+    [SerializeField]
+    private AudioClip waterWalk;
+    // add more audio clips as needed
+
+    public TerrainType CurrentTerrain { get; set; } = TerrainType.Grass;
+
+    private AudioSource walkSource;
+    private CharacterController characterController;
+
+    private void Start()
     {
-        
+        walkSource = GetComponent<AudioSource>();
+        characterController = GetComponent<CharacterController>();
+    }
+
+    private void Update()
+    {
+        switch (CurrentTerrain)
+        {
+            case TerrainType.Grass:
+                walkSource.clip = grassWalk;
+                break;
+            case TerrainType.Wood:
+                walkSource.clip = woodWalk;
+                break;
+            case TerrainType.Sand:
+                walkSource.clip = sandWalk;
+                break;
+            case TerrainType.Water:
+                walkSource.clip = waterWalk;
+                break;
+                // add more cases as needed
+        }
+
+        if (!walkSource.isPlaying && characterController.velocity.magnitude > 0.1f)
+        {
+            walkSource.Play();
+        }
+        else if (walkSource.isPlaying && characterController.velocity.magnitude < 0.1f)
+        {
+            walkSource.Stop();
+        }
     }
 }
